24abc7c5067d030c5f82dbb67b7ee309d80e4a14
Florian BUREL <florian.burel@mac.com>
2014-07-10 10:13:08 +0200

    Creation du cityTableViewController

---
+++ MyFlickr/CityTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CityTableViewController.h
+//  MyFlickr
+//
+//  Created by Florian BUREL on 10/07/2014.
+//  Copyright (c) 2014 florian burel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CityTableViewController : UIViewController
+
+@end
---
+++ MyFlickr/CityTableViewController.m
@@ -0,0 +1,30 @@
+//
+//  CityTableViewController.m
+//  MyFlickr
+//
+//  Created by Florian BUREL on 10/07/2014.
+//  Copyright (c) 2014 florian burel. All rights reserved.
+//
+
+#import "CityTableViewController.h"
+
+@interface CityTableViewController ()
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
+
+@implementation CityTableViewController
+
+- (IBAction)addCity:(id)sender
+{
+    
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
