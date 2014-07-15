53db873b6df33692ffdfbc91e7e7c68b67dba251
Florian BUREL <florian.burel@mac.com>
2014-07-15 16:31:56 +0200

    Fin

--- MyFlickr/Localizer.m
+++ MyFlickr/Localizer.m
@@ -9,22 +9,95 @@
 #import "Localizer.h"
 
 #import "City.h"
+
 @import CoreLocation;
+
+@interface Localizer () <CLLocationManagerDelegate>
+
+@property (strong, nonatomic) City * city;
+@property (strong, nonatomic) CLLocationManager * locationManager;
+
+@end
+
+
 
 @implementation Localizer
 
+#pragma mark - Singleton
 
-+ (instancetype)sharedInstance {
-    static id __SharedInstance = nil;
++ (instancetype)sharedInstance
+{
+    static id sharedInstance = nil;
+    
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        __SharedInstance = [[self alloc] init];
+        sharedInstance = [self new];
     });
-    return __SharedInstance;
+    
+    return sharedInstance;
 }
 
 - (void)startUpdatingLocationForCity:(City *)city
 {
     
+    self.city = city;
+    
+    [self.locationManager  startUpdatingLocation];
+    
 }
+
+- (CLLocationManager *)locationManager
+{
+    if(!_locationManager)
+    {
+        _locationManager = [CLLocationManager new];
+        _locationManager.delegate = self;
+        _locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers;
+    }
+    
+    return _locationManager;
+
+}
+
+
+#pragma mark - CLLocationManagerDelegate
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+    
+    CLLocation * lastLocation = [locations lastObject];
+    
+    self.city.longitude = @(lastLocation.coordinate.longitude);
+    self.city.latitude = @(lastLocation.coordinate.latitude);
+    
+    [self.locationManager stopUpdatingLocation];
+    
+    
+    // GEOCODING
+    
+    CLGeocoder * gc = [CLGeocoder new];
+    [gc reverseGeocodeLocation:lastLocation
+             completionHandler:^(NSArray *placemarks, NSError *error) {
+        CLPlacemark * placemark = [placemarks lastObject];
+        self.city.name = placemark.locality;
+
+    }];
+}
+
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
